282a74d1ccd848e5e29ca492be55d3742c9edc85
Daniel.Montivero <montivero.daniel.e@gmail.com>
2020-04-16 21:37:20 -0300

    Change java first commit

---
+++ change-java.bat
@@ -0,0 +1,52 @@
+@echo off
+
+
+REM https://sites.google.com/site/eneerge/scripts/batchgotadmin
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+
+    echo "B"
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+
+    echo %OUTPUT_MESSAGE%
+
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+    echo "C"
+:--------------------------------------   
+
+SET SCRIPT_DIR=%~dp0
+SET FILE_VERSION=%SCRIPT_DIR%/java-location.txt
+SET JAVA_TARGET=%1
+
+for /F "eol=# delims== tokens=1,*" %%a in (%FILE_VERSION%) do (
+
+    IF "%%a" == "%JAVA_TARGET%" (
+        SET OUTPUT_MESSAGE="Change to use %%b Restart your console/app to take effect"
+        SETX /M JAVA_HOME "%%b"
+    )
+
+)
